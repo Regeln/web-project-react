ee876b6fca9d99f058e3ecc347becfcd2412a298
Regeln <regeln17@protonmail.com>
2023-04-01 22:41:31 +0300

    update items.js

--- src/items.js
+++ src/items.js
@@ -2,21 +2,21 @@ export default {
     products : [
         {
             _id: "1",
-            name: "Pepperoni",
+            name: "Папероні",
             category: "pizza",
             image: "/images/p1.jpg",
             price: 150
         },
         {
             _id: "2",
-            name: "Americano",
+            name: "Американа",
             category: "pizza",
             image: "images/p2.jpg",
             price: 140
         },
         {
             _id: "3",
-            name: "Prosciutto",
+            name: "Прошуто",
             category: "pizza",
             image: "images/p3.jpg",
             price: 120
