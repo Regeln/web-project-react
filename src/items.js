c59aeb6d1b37377ab0a1ee23af338aef36e8498d
Regeln <regeln17@protonmail.com>
2023-04-01 22:35:10 +0300

    add items.js file

---
+++ src/items.js
@@ -0,0 +1,25 @@
+export default {
+    items : [
+        {
+            _id: "1",
+            name: "Pepperoni",
+            category: "pizza",
+            image: "/images/p1.jpg",
+            price: 150
+        },
+        {
+            _id: "2",
+            name: "Americano",
+            category: "pizza",
+            image: "images/p2.jpg",
+            price: 140
+        },
+        {
+            _id: "3",
+            name: "Prosciutto",
+            category: "pizza",
+            image: "images/p3.jpg",
+            price: 120
+        }
+    ]
+}
