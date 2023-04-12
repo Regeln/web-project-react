8c89646f5e77ccc0b091ae4aa437505a44e9f8b6
Regeln <regeln17@protonmail.com>
2023-04-12 05:36:23 +0300

    Make header a component, add routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
-
-
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ProductsListPage from "./pages/ProductsListPage";
+import CartPage from "./pages/CartPage";
+import Header from "./components/Header";
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(
+        !!localStorage.getItem("userToken")
+    );
+
     return (
         <BrowserRouter>
             <div className="grid-container">
-                <header className="header">
-                    <div className="header-menu">
-                        <Link to="/">foodLike</Link>
-                    </div>
-                    <div className="header-links">
-                        <a href="signin.html">Реєстрація</a>
-                        <a href="cart.html">Корзина</a>
-                    </div>
-                </header>
+                <Header />
                 <section>
                     <div className="content">
                         <Routes>
-                            <Route path="/product/:id" element={<ProductPage />} />
+                            {isLoggedIn && 
+                                <>
+                                    <Route path="/cart" element={<CartPage />} />
+                                    <Route path="/category/:id" element={<ProductsListPage />} />
+                                    <Route path="/product/:id" element={<ProductPage />} />
+                                </> 
+                            }
+                            <Route path="/signup" element={<SignupPage />} />
+                            <Route path="/login" element={<LoginPage />} /> 
                             <Route path="/" element={<HomePage />} />
                         </Routes>
                     </div>
