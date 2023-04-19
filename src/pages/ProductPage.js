51e37921ff5469f284d992505c86fa62317d9114
Regeln <regeln17@protonmail.com>
2023-04-19 08:21:07 +0300

    Update product page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,14 +1,40 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import items from "../items";
 
 function ProductPage() {
     const params = useParams();
-    const product = items.products.find(item => item._id === params.id);
+    const [product, setProduct] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const loadProduct = async () => {
+            const res = await fetch(`http://localhost:4000/product/${params.id}`)
+            const data = await res.json();
+    
+            //! TODO bad response handling
+            if (res.status >= 400)
+                return;
+            
+            setProduct(data);
+            setIsLoading(false);
+        }
+
+        loadProduct();
+    }, [params.id]);
+
+    const handleAddToCart = () => {
+        console.log(product);
+        alert("Товар добавлено в корзину");
+    }
+
+
+    if (isLoading) {
+        return null;
+    }
+
     return (
         <div className="product-content">
-            <div className="product-image">
-                <img src={product.image} alt="" />
-            </div>
+            <img className="product-image" src={product.image_path} alt={product.category + " image"} />
             <div className="product-info">
                 <h2 className="product-info-name">{product.name}</h2>
                 <div className="product-info-price">
@@ -19,7 +45,7 @@ function ProductPage() {
                         Description product id: {product._id}. 
                     </p>
                 </div>
-                <button className="product-info-add-to-cart" type="button">
+                <button className="product-info-add-to-cart" onClick={handleAddToCart} type="button">
                     Додати до кошику
                 </button>
             </div>
